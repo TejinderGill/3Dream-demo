cbdae03edd1a15c045163e4085e8b184606bc92b
tejindergill91 <laddi_122005@hotmail.com>
2015-08-30 21:22:41 -0400

    added footer
    added footer

--- footer.php
+++ footer.php
@@ -1,21 +1,24 @@
  <!-- footer -->
-        <footer class="footer text-center">
+        <footer class="footer">
+           <div class="lines">
+            <p>The best online 3D space planning software for interior designers, decorators, furniture retailers, homeowners and anyone wishing to create realistic living spaces fast and easy. Plan in 2D and switch to 3D for real-time rendering (just like a video game) to complete your detailed room design with finishes and decorative accessories.</p>
+            </div>
             <div class="col-md-4">
-                <ul>
+                <ul class="list">
                     <li> <a href="">Careers</a> </li>
                     <li> <a href="">SiteMap</a> </li>
                     <li> <a href="">Contact</a> </li>
                 </ul>
             </div>
             <div class="col-md-4">
-                <ul>
+                <ul class="list">
                     <li> <a href="">Events</a> </li>
                     <li> <a href="">Gift Cards</a> </li>
                     <li> <a href="">Contents</a> </li>
                 </ul>
             </div>
             <div class="col-md-4">
-                <ul>
+                <ul class="list">
                 </ul>
             </div>
             <div class="footer-container">
